2633d4dca05b4bdfc5c28e31cf3bebbbae75406d
Eduardo Marin <du.marin10@gmail.com>
2022-12-03 01:50:39 -0300

    Ex.01-Calculadora_Automatica

---
+++ Calculadora automatica/main.py
@@ -0,0 +1,18 @@
+print('Exercicio Calculadora')
+
+number_1 = float(input('Insira o primeiro numero: '))
+number_2 = float(input('Insira o segundo numero: '))
+
+print('{} + {} = '.format(number_1,number_2))
+print(number_1 + number_2)
+
+print('{} - {} = '.format(number_1,number_2))
+print(number_1 - number_2)
+
+print('{} x {} = '.format(number_1,number_2))
+print(number_1 * number_2)
+
+print('{} / {} = '.format(number_1,number_2))
+print(number_1 / number_2)
+
+
